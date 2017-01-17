d7e1986ed693925eb11e567453a14cb2662e94d5
ChangWeiTan <cwtan229@gmail.com>
2017-01-17 14:53:31 +1100

    Update ClassifierNNED.java

--- src/classifiers/ClassifierNNED.java
+++ src/classifiers/ClassifierNNED.java
@@ -21,179 +21,147 @@ import java.util.ArrayList;
 import similaritymeasures.DistanceMeasures;
 
 public class ClassifierNNED {
-	/* NN-ED Classifier
-	 * This is a class for NN-ED classifier
+	/* NNED Classifier
 	 * 
-	 * Last modified: 12/01/2017
+	 * Last modified: 01/10/2016
 	 */
 	private final DistanceMeasures distComputer = new DistanceMeasures();	// computer for distance measures
-	private final int NIL = -100;											// constant for not available								
-	private int printInterval;												// interval for printing 
-	private int steps;														// steps to record error (steps=1, means record error for each training example)
-	private int nbSteps;													// number of steps
-	private long startTime, stopTime, saveTime;								// start, stop, save time per query
-	private double totalQueryTime, elapsedTime;								// total query time, elapsed time = time per query
+	private final int nil = -100;					// constant for not available								
+	private int printInterval;						// interval for printing 
+	private int steps;								// steps to record error (steps=1, means record error for each training example)
+	private int intervals;							// time intervals
+	private long startTime, stopTime, saveTime;		// start stop and save time per query
+	private double totalQueryTime, elapsedTimePerQuery;		// total query time, elapsed time = time per query
 	
-	private ArrayList<double[]> TRAIN;		// training dataset
-	private ArrayList<Integer> TrainClass;	// classes for the training dataset
-	private ArrayList<Integer> TrainIndex;	// index for the training dataset
-	private int TrainSize;					// size of training dataset
-	private int ActualClass;				// actual class of the query
-	private int predictClass; 
+	private ArrayList<double[]> trainingDataset;		// training dataset
+	private ArrayList<Integer> trainingDatasetClass;	// classes for the training dataset
+	private ArrayList<Integer> trainingDatasetIndex;	// index for the training dataset
+	private int trainingDatasetSize;					// size of training dataset
+	private int actualClass;							// actual class of the query
 	
-	private static double errorRate;			// error rate 
-	private static double distComputation = 0;	// distance computations
-	private static double averageQueryTime = 0;	// average query time
-	private double[] distance1NN;				// distance to NN for each query
-	private int[] index1NN;						// nearest neighbour for each query
+	private double errorRate;						// error rate 
+	private double distComputation = 0;				// distance computations
+	private double averageQueryTime = 0;				// average query time
+	private double[] distance1NN;							// distance recorded for each query
+	private int[] index1NN;									// nearest neighbour index for each query
 	
-	private int bsfIndex;						// best so far nearest neighbour index
-	private double bsfDist;						// best so far distance
+	private int bsfIndex;									// best so far nearest neighbour index
+	private double bsfDist;									// best so far distance
 	
-	private double[] averageErrorPerQuery;		// average error per query
-	private double[] averageDistPerQuery;		// average distance computations per query
-	private double[] averageTimePerQuery;		// average time per query
+	private double[] averageErrorPerQuery;					// average error per query
+	private double[] averageDistPerQuery;					// average distance computations per query
+	private double[] averageTimePerQuery;					// average time per query
 	
-	private double[] errorPerQuery;				// error per query at different time intervals
-	private double[] timePerQuery;				// time per query at different time intervals
-	private double[] distPerQuery;				// distance computations per query at different time intervals
-	private int[] seenSoFarPerQuery;			// number of time series seen so far
+	private double[] errorPerQuery;							// error per query at different time intervals
+	private double[] timePerQuery;							// time per query at different time intervals
+	private double[] distPerQuery;							// distance computations per query at different time intervals
+	private int[] seenSoFarPerQuery;						// number of time series seen so far
 	
 	public ClassifierNNED(){}
 	
 	public ClassifierNNED(final ArrayList<double[]> D, final ArrayList<Integer> dataClass, final ArrayList<Integer> index, final int step, final int stepSize) {
-		TRAIN = D;
-		TrainClass = dataClass;
-		TrainSize = D.size();
-		TrainIndex = index;
+		trainingDataset = D;
+		trainingDatasetClass = dataClass;
+		trainingDatasetSize = D.size();
+		trainingDatasetIndex = index;
 		steps = step;
-		nbSteps = stepSize;
-		printInterval = (int) (0.1 * TrainSize);
+		intervals = stepSize;
+		printInterval = (int) 1;
 		distComputation = 0;
 	}
-	
-	/* Methods */
-	public final double performance(final ArrayList<double[]> testSet, final ArrayList<Integer> testClass) {
-		/* Performance
-		 * This is used to validate the performance of the classifier using a testing dataset.
-		 * We record the error rate at different time interval
-		 * Inputs: 
-		 * 	testSet		: Testing dataset
-		 * 	testClass	: Class for testing dataset
-		 * Output: 
-		 * 	errorRate 	: Error rate of the validation
-		 */
-		final int testSize = testClass.size();	// size of testing dataset
-		int predictClass;						// predict class of the query
-		int errorCount = 0;						// counter for errors
-		int i;									// for loop counter
-		double errorSoFar;						// error rate so far
 		
-		errorRate = NIL;
+	public final double performance(final ArrayList<double[]> testSet, final ArrayList<Integer> testClass) {
+		int errorCount = 0;						
+		
+		// initialize
 		totalQueryTime = 0;
 		distComputation = 0;
-		distance1NN = new double[testSize];
-		index1NN = new int[testSize];
+		distance1NN = new double[testSet.size()];
+		index1NN = new int[testSet.size()];
 		
 		// initialize the results for different time intervals
-		seenSoFarPerQuery = new int[nbSteps];
-		errorPerQuery = new double[nbSteps];
-		timePerQuery = new double[nbSteps];
-		distPerQuery = new double[nbSteps];
-		averageErrorPerQuery = new double[nbSteps];
-		averageTimePerQuery = new double[nbSteps];
-		averageDistPerQuery = new double[nbSteps];
+		seenSoFarPerQuery = new int[intervals];
+		errorPerQuery = new double[intervals];
+		timePerQuery = new double[intervals];
+		distPerQuery = new double[intervals];
+		averageErrorPerQuery = new double[intervals];
+		averageTimePerQuery = new double[intervals];
+		averageDistPerQuery = new double[intervals];
 		
-		for (i = 0; i < testSize; i++){
+		for (int i = 0; i < testSet.size(); i++){
 			// print status after certain intervals
 			if (i % printInterval == 0) {
 				System.out.print("Classifying Test " + (i+1) + ", ");
 			}
 			
-			ActualClass = testClass.get(i);				// actual class of the query
-			predictClass = classify(testSet.get(i), i); // predicted class of the query
-			totalQueryTime += elapsedTime;				// update total query time
+			actualClass = testClass.get(i);							// actual class of the query
+			final int predictClass = classify(testSet.get(i), i); 	// predicted class of the query
+			totalQueryTime += elapsedTimePerQuery;					// update total query time
 			
 			// update error count
-			if (predictClass != ActualClass) {
+			if (predictClass != actualClass) {
 				errorCount++;
 			}
 			
 			// update error rate so far & store the best so far distance & nearest neighbour index
-			errorSoFar = (double) errorCount/(i+1);
+			final double errorSoFar = (double) errorCount/(i+1);
 			distance1NN[i] = bsfDist;
 			index1NN[i] = bsfIndex;
 			
 			// print status after certain intervals
 			if (i % printInterval == 0) {
-				System.out.println("took " + elapsedTime + "s, NN Index: " + bsfIndex + ", Error so far: " + errorSoFar);
+				System.out.println("took " + elapsedTimePerQuery + "s, NN Index: " + bsfIndex + ", Error so far: " + errorSoFar);
 			}
 		}
 		
 		// compute error rate, number of distance computations & average query time per query at different time intervals
-		for (i = 0; i < nbSteps; i++) {
-			averageErrorPerQuery[i] = (double) errorPerQuery[i]/testSize;
-			averageTimePerQuery[i] = (double) timePerQuery[i]/testSize;
-			averageDistPerQuery[i] = (double) distPerQuery[i]/testSize;
+		for (int i = 0; i < intervals; i++) {
+			averageErrorPerQuery[i] = (double) errorPerQuery[i]/testSet.size();
+			averageTimePerQuery[i] = (double) timePerQuery[i]/testSet.size();
+			averageDistPerQuery[i] = (double) distPerQuery[i]/testSet.size();
 		}
 		
 		// compute error rate, number of distance computations & average query time
-		errorRate = (double) errorCount/testSize;
-		distComputation = (double) distComputation/testSize;
-		averageQueryTime = (double) totalQueryTime/testSize;
+		errorRate = (double) errorCount/testSet.size();
+		distComputation = (double) distComputation/testSet.size();
+		averageQueryTime = (double) totalQueryTime/testSet.size();
 		
 		return errorRate;
 	}
-		
-	private final int classify(final double[] Q, final int testNum) {
-		/* Classify
-		 * This is used to classify a query time series using NN-ED
-		 * We record the error rate at different time interval
-		 * Inputs: 
-		 * 	Q				: Query time series
-		 * 	testNum			: Query number, use for recording the error rates, only record when we are classifying for the first time
-		 * Output: 
-		 * 	predictClass 	: Predicted class for that query
-		 */
-		int i;					// for loop counter
-		int count = 0; 			// counter for storing the results at different time intervals
-		long n = 0;				// distance computations for the experiment
-		double edDistance;		// dtw and lb distances
-		double[] C;				// candidate time series
+	
+	private final int classify(final double[] query, final int testNum) {
+		int predictClass = nil;
+		int count = 0; 					
+		long distComputationPerQuery = 0;		
 
-		predictClass = NIL;
-		bsfIndex = NIL;
+		// initialize
+		bsfIndex = nil;
 		bsfDist = Double.POSITIVE_INFINITY;
-		elapsedTime = 0;
+		elapsedTimePerQuery = 0;
 		startTime = System.nanoTime();
 		saveTime = 0;
 		
-		// go through the whole training dataset
-		for (i = 0; i < TrainSize; i++) {
-			C = TRAIN.get(i);
-			// compute lower-bound distance
-			edDistance = distComputer.ed(Q, C);
-			distComputation += distComputer.getLBDistComputation();
-			n += distComputer.getLBDistComputation();
+		for (int i = 0; i < trainingDatasetSize; i++) {
+			final double edDistance = distComputer.ed(query, trainingDataset.get(i));
+			distComputationPerQuery += distComputer.getLBDistComputation();
 			
 			if (edDistance < bsfDist) {
-				// update nearest candidate if ed is smaller than best so far
-				bsfIndex = TrainIndex.get(i);;
-				predictClass = TrainClass.get(i);
+				predictClass = trainingDatasetClass.get(i);
+				bsfIndex = trainingDatasetIndex.get(i);
 				bsfDist = edDistance;
 			}
 			stopTime = System.nanoTime();
 			
-			if (i == 0 || i == (TrainSize-1) || Math.floorMod(i+1, steps) == 0) {
+			if (i == 0 || i == (trainingDatasetSize-1) || Math.floorMod(i+1, steps) == 0) {
 				// record error rate at fixed time intervals/number of time series seen
-				elapsedTime = (double) (stopTime - startTime - saveTime)/1e9;
-
-				if (predictClass != ActualClass) {
+				elapsedTimePerQuery = (double) (stopTime - startTime - saveTime)/1e9;
+				
+				if (predictClass != actualClass) {
 					errorPerQuery[count]++;
 				}
-
-				timePerQuery[count] += elapsedTime;
-				distPerQuery[count] += n;
+				
+				timePerQuery[count] += elapsedTimePerQuery;
+				distPerQuery[count] += distComputationPerQuery;
 				if (testNum == 0) {
 					seenSoFarPerQuery[count] = i+1;
 				}
@@ -204,20 +172,21 @@ public class ClassifierNNED {
 		
 		// compute time per query
 		stopTime = System.nanoTime();
-		elapsedTime = (double) (stopTime - startTime - saveTime)/1e9;
+		elapsedTimePerQuery = (double) (stopTime - startTime - saveTime)/1e9;
+		distComputation += distComputationPerQuery;
 		
 		return predictClass;
 	}
 	
-	public final double getErrorRate() {
+	public double getErrorRate() {
 		return errorRate;
 	}
 	
-	public final double getDistComputation() {
+	public double getDistComputation() {
 		return distComputation;
 	}
 		
-	public final double getAverageQueryTime() {
+	public double getAverageQueryTime() {
 		return averageQueryTime;
 	}
 	
