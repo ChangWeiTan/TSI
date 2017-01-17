7502edd5d7d2c940bee81f6393e933cb36b60840
ChangWeiTan <cwtan229@gmail.com>
2017-01-17 15:06:24 +1100

    Add files via upload

--- src/datasets/Dataset.java
+++ src/datasets/Dataset.java
@@ -30,9 +30,8 @@ import tools.Tools;
 
 public class Dataset {
 	/* Dataset
-	 * This is a class for time series dataset
 	 * 
-	 * Last modified: 12/01/2017
+	 * Last modified: 14/01/2017
 	 */
 	private static final Tools tools = new Tools();	// tools for the functions
 	private static String datasetName;				// dataset name
@@ -42,31 +41,23 @@ public class Dataset {
 	private static int sizeTest;					// size of testing dataset
 	private static int warpingWindow;				// warping window size
 										
-	private static ArrayList<double[]> TRAIN;		// training dataset
-	private static ArrayList<double[]> TEST;		// testing dataset
-	private static ArrayList<Integer> TrainClass;	// class of training dataset
-	private static ArrayList<Integer> TestClass;	// class of testing dataset
-	private static ArrayList<Integer> TrainIndex;	// index of training dataset
-	private static ArrayList<Integer> TestIndex;	// index of testing dataset
+	private static ArrayList<double[]> trainingDataset;		// training dataset
+	private static ArrayList<double[]> testingDataset;		// testing dataset
+	private static ArrayList<Integer> trainingDatasetClass;	// class of training dataset
+	private static ArrayList<Integer> testingDatasetClass;	// class of testing dataset
+	private static ArrayList<Integer> trainingDatasetIndex;	// index of training dataset
+	private static ArrayList<Integer> testingDatasetIndex;	// index of testing dataset
 		
 	private ArrayList<double[]> randDataset;		// random dataset
 	private int[] randIndex;						// index of random dataset
 	private ArrayList<Integer> randClass;			// class of random dataset
-	
-	private ArrayList<double[]> subDataset;			// sub dataset 
-	private ArrayList<Integer> subClass;			// class of sub dataset
 	
 	/* Constructors */
 	public Dataset(final String datasetname) {
 		datasetName = datasetname;
 	}
 	
-	/* Method */
-	public final void loadTest(final String path) {
-		/* Load testing dataset 
-		 * Inputs: 
-		 * 	path	: project path 
-		 */
+	public final void loadTestSet(final String path) {
 		getDatasetProperties(path);	// get dataset properties, number of class, warping window etc
 		
 		// testing dataset file
@@ -74,16 +65,12 @@ public class Dataset {
 		
 		// read dataset
 		tools.readDatasetCSV(testFile, sizeTest);
-		TestIndex = tools.getDataIndexAftRead();
-		TestClass = tools.getDataLabelAftRead();
-		TEST = tools.getTimeseriesAftRead();
+		testingDatasetIndex = tools.getDataIndexAftRead();
+		testingDatasetClass = tools.getDataLabelAftRead();
+		testingDataset = tools.getTimeseriesAftRead();
 	}
 	
-	public final void loadTestcsv(final String path) {
-		/* Load testing dataset in csv
-		 * Inputs: 
-		 * 	path	: project path 
-		 */
+	public final void loadTestSetCSV(final String path) {
 		getDatasetProperties(path);	// get dataset properties, number of class, warping window etc
 		
 		// testing dataset file
@@ -91,16 +78,12 @@ public class Dataset {
 		
 		// read dataset from csv
 		tools.readDatasetCSV(testCSV, sizeTest);
-		TestIndex = tools.getDataIndexAftRead();
-		TestClass = tools.getDataLabelAftRead();
-		TEST = tools.getTimeseriesAftRead();
+		testingDatasetIndex = tools.getDataIndexAftRead();
+		testingDatasetClass = tools.getDataLabelAftRead();
+		testingDataset = tools.getTimeseriesAftRead();
 	}
 	
-	public final void loadTrain(final String path) {
-		/* Load training dataset 
-		 * Inputs: 
-		 * 	path	: project path 
-		 */
+	public final void loadTrainSet(final String path) {
 		getDatasetProperties(path);	// get dataset properties, number of class, warping window etc
 		
 		// training dataset file
@@ -108,16 +91,12 @@ public class Dataset {
 		
 		// read dataset from csv
 		tools.readDatasetCSV(trainFile, sizeTest);
-		TrainIndex = tools.getDataIndexAftRead();
-		TrainClass = tools.getDataLabelAftRead();
-		TRAIN = tools.getTimeseriesAftRead();
+		trainingDatasetIndex = tools.getDataIndexAftRead();
+		trainingDatasetClass = tools.getDataLabelAftRead();
+		trainingDataset = tools.getTimeseriesAftRead();
 	}
 	
-	public final void loadTraincsv(final String path) {
-		/* Load training dataset in csv 
-		 * Inputs: 
-		 * 	path	: project path 
-		 */
+	public final void loadTrainSetCSV(final String path) {
 		getDatasetProperties(path);	// get dataset properties, number of class, warping window etc
 		
 		// training dataset file
@@ -125,18 +104,13 @@ public class Dataset {
 		
 		// read dataset from csv
 		tools.readDatasetCSV(trainCSV, sizeTest);
-		TrainIndex = tools.getDataIndexAftRead();
-		TrainClass = tools.getDataLabelAftRead();
-		TRAIN = tools.getTimeseriesAftRead();
+		trainingDatasetIndex = tools.getDataIndexAftRead();
+		trainingDatasetClass = tools.getDataLabelAftRead();
+		trainingDataset = tools.getTimeseriesAftRead();
 	}
 	
-	public final void saveIndex1NN(final int[] index1NN, final String projectPath) {
-		/* Save Index 1NN
-		 * Inputs: 
-		 * 	index1NN	: 1NN index
-		 * 	projectPath	: project path 
-		 */
-		final String csvFile = projectPath + datasetName + "_1NN_LB_index1NN.csv";	// file name
+	public final void save1NNIndex(final int[] index1NN, final String projectPath) {
+		final String csvFile = projectPath + datasetName + "_1NN_LB_index1NN.csv";	
 		BufferedWriter br = null;
 		
 		try {
@@ -151,15 +125,9 @@ public class Dataset {
 	}
 	
 	public final int[] read1NNIndex(final String path) {
-		/* Read the actual NN Index
-		 * Inputs: 
-		 * 	path		: project path 
-		 * Output:
-		 * 	index1NN	: 1NN index
-		 */
-		final int[] index1NN = new int[sizeTest];		// 1NN index
-		final String csvSplitby = ",";					// delimiter
-		final String csvFile = path + datasetName + "_1NN_LB_index1NN.csv";	// file name
+		final int[] index1NN = new int[sizeTest];		
+		final String delimiter = ",";					
+		final String csvFile = path + datasetName + "_1NN_LB_index1NN.csv";	
 		String line = "";
 		BufferedReader br = null;
 		
@@ -169,7 +137,7 @@ public class Dataset {
 			br = new BufferedReader(new FileReader(csvFile));
 			while ((line = br.readLine()) != null) 
 			{
-				final String[] data = line.split(csvSplitby);
+				final String[] data = line.split(delimiter);
 				index1NN[count++] = Integer.parseInt(data[0]);
 			}
 		} catch (FileNotFoundException e) {
@@ -190,11 +158,7 @@ public class Dataset {
 	}
 	
 	private final void getDatasetProperties(final String path) {
-		/* Get dataset properties
-		 * Inputs: 
-		 * 	path		: project path 
-		 */
-		final String filename = path + datasetName + "/" + datasetName + "_properties.csv";	// file name
+		final String filename = path + datasetName + "/" + datasetName + "_properties.csv";	
 		final String csvSplitby = ",";	// delimiter
 		String line = "";	
 		BufferedReader br = null;
@@ -224,121 +188,74 @@ public class Dataset {
 		}
 	}	
 	
-	public final int[] randomize(final ArrayList<double[]> dataset, final ArrayList<Integer> label, final ArrayList<Integer> index) {
-		/* Randomize the given dataset 
-		 * it returns an integer array
-		 * Inputs: 
-		 * 	dataset		: Dataset to be randomized
-		 * 	label		: Class of the dataset
-		 * 	index 		: Index of the dataset
-		 * Output:
-		 * 	randIndex	: Randomized index
-		 */
+	public final int[] randomize(final ArrayList<double[]> dataset, final ArrayList<Integer> dataClass, final ArrayList<Integer> dataIndex) {
 		System.out.println("Randomizing dataset");
 		
-		final int NumTimeseries = dataset.size();	// size of dataset
-		
 		// temporary data index
-		final ArrayList<Integer> list = new ArrayList<Integer>(NumTimeseries);
-		list.addAll(index);
+		final ArrayList<Integer> tempData = new ArrayList<Integer>(dataset.size());
+		tempData.addAll(dataIndex);
 		
 		// initialize
-		randDataset = new ArrayList<double[]>(NumTimeseries);
-		randClass = new ArrayList<Integer>(NumTimeseries);
-		randIndex = new int[NumTimeseries]; 
-		int count = 0;
+		randDataset = new ArrayList<double[]>(dataset.size());
+		randClass = new ArrayList<Integer>(dataset.size());
+		randIndex = new int[dataset.size()]; 
+		int dataCount = 0;
 		
-		while (list.size() > 0) 
-		{
+		while (tempData.size() > 0) {
 			final Random rand = new Random();
-			final int idx = rand.nextInt(list.size());
-			randIndex[count] = list.get(idx);
-			randDataset.add(dataset.get(randIndex[count]));
-			randClass.add(label.get(randIndex[count]));
-			count++;
-			list.remove(idx);
+			final int idx = rand.nextInt(tempData.size());
+			randIndex[dataCount] = tempData.get(idx);
+			randDataset.add(dataset.get(randIndex[dataCount]));
+			randClass.add(dataClass.get(randIndex[dataCount]));
+			dataCount++;
+			tempData.remove(idx);
 		}
 		
 		return randIndex;
 	}
 	
-	public final ArrayList<Integer> randomize2(final ArrayList<double[]> dataset, final ArrayList<Integer> label, final ArrayList<Integer> index) {
-		/* Randomize the given dataset and returns arraylist
-		 * it returns an arraylist
-		 * Inputs: 
-		 * 	dataset		: Dataset to be randomized
-		 * 	label		: Class of the dataset
-		 * 	index 		: Index of the dataset
-		 * Output:
-		 * 	randIndex	: Randomized index
-		 */
+	public final ArrayList<Integer> randomize2(final ArrayList<double[]> dataset, final ArrayList<Integer> dataClass, final ArrayList<Integer> dataIndex) {
 		System.out.println("Randomizing dataset");
 		
-		final int NumTimeseries = index.size();	// size of dataset
-		
 		// temporary data index
-		final ArrayList<Integer> list = new ArrayList<Integer>(NumTimeseries);
-		list.addAll(index);
+		final ArrayList<Integer> tempData = new ArrayList<Integer>(dataset.size());
+		tempData.addAll(dataIndex);
 		
 		// initialize
-		randDataset = new ArrayList<double[]>(NumTimeseries);
-		randClass = new ArrayList<Integer>(NumTimeseries);
-		ArrayList<Integer> randIndex = new ArrayList<Integer>(NumTimeseries); 
-		int count = 0;
+		randDataset = new ArrayList<double[]>(dataset.size());
+		randClass = new ArrayList<Integer>(dataset.size());
+		ArrayList<Integer> randIndex = new ArrayList<Integer>(dataset.size()); 
+		int dataCount = 0;
 		
-		while (list.size() > 0) {
+		while (tempData.size() > 0) {
 			final Random rand = new Random();
-			final int idx = rand.nextInt(list.size());
-			randIndex.add(list.get(idx));
-			randDataset.add(dataset.get(randIndex.get(count)));
-			randClass.add(label.get(randIndex.get(count)));
-			count++;
-			list.remove(idx);
+			final int idx = rand.nextInt(tempData.size());
+			randIndex.add(tempData.get(idx));
+			randDataset.add(dataset.get(randIndex.get(dataCount)));
+			randClass.add(dataClass.get(randIndex.get(dataCount)));
+			dataCount++;
+			tempData.remove(idx);
 		}
 		
 		return randIndex;
 	}
 	
-	public final ArrayList<double[]> extractDataset(final ArrayList<double[]> dataset, final int[] index) {
-		/* Extract dataset from the given index
-		 * Inputs: 
-		 * 	dataset		: Dataset to be randomized
-		 * 	index 		: Index to be selected
-		 * Output:
-		 * 	newDataset	: Extracted dataset
-		 */
-		final int datasetSize = index.length;
-		final ArrayList<double[]> newDataset = new ArrayList<double[]>(datasetSize);
-		for (int i = 0; i < datasetSize; i++) {
-			newDataset.add(dataset.get(index[i]));
+	public final ArrayList<double[]> extractDataset(final ArrayList<double[]> dataset, final int[] dataIndex) {
+		final ArrayList<double[]> newDataset = new ArrayList<double[]>(dataIndex.length);
+		for (int i = 0; i < dataIndex.length; i++) {
+			newDataset.add(dataset.get(dataIndex[i]));
 		}
 		
 		return newDataset;
 	}
 	
-	public final ArrayList<Integer> extractLabel(final ArrayList<Integer> dataset, final int[] index) {
-		/* Extract dataset label from the given index
-		 * Inputs: 
-		 * 	dataset		: Dataset to be randomized
-		 * 	index 		: Index to be selected
-		 * Output:
-		 * 	newLabel	: Extracted dataset label
-		 */
-		final int datasetSize = index.length;
-		final ArrayList<Integer> newLabel = new ArrayList<Integer>(datasetSize);
-		for (int i = 0; i < datasetSize; i++) {
-			newLabel.add(dataset.get(index[i]));
+	public final ArrayList<Integer> extractLabel(final ArrayList<Integer> dataset, final int[] dataIndex) {
+		final ArrayList<Integer> newLabel = new ArrayList<Integer>(dataIndex.length);
+		for (int i = 0; i < dataIndex.length; i++) {
+			newLabel.add(dataset.get(dataIndex[i]));
 		}
 		
 		return newLabel;
-	}
-	
-	public final ArrayList<double[]> getSubDataset() {
-		return subDataset;
-	}
-	
-	public final ArrayList<Integer> getSubLabel() {
-		return subClass;
 	}
 	
 	public final ArrayList<double[]> getRandDataset() {
@@ -374,26 +291,26 @@ public class Dataset {
 	}
 	
 	public final ArrayList<double[]> TrainSet() {
-		return TRAIN;
+		return trainingDataset;
 	}
 	
 	public final ArrayList<double[]> TestSet() {
-		return TEST;
+		return testingDataset;
 	}
 	
 	public final ArrayList<Integer> ClassTrain() {
-		return TrainClass;
+		return trainingDatasetClass;
 	}
 	
 	public final ArrayList<Integer> ClassTest() {
-		return TestClass;
+		return testingDatasetClass;
 	}
 	
 	public final ArrayList<Integer> IndexTrain() {
-		return TrainIndex;
+		return trainingDatasetIndex;
 	}
 	
 	public final ArrayList<Integer> IndexTest() {
-		return TestIndex;
+		return testingDatasetIndex;
 	}
 }
